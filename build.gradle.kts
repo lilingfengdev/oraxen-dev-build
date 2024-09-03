0d85897e8ee66a488cd01f885b3a713002ecb90c
Lincey <2365638692@qq.com>
2024-09-03 21:12:14 +0800

    Fix incorrect dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ allprojects {
 
         implementation("com.github.stefvanschie.inventoryframework:IF_Folia:0.10.14-SNAPSHOT")
         implementation(files("../libs/compile-folia/custom-block-data-2.2.2.jar"))
-        implementation("com.jeff_media:MorePersistentDataTypes:2.4.0")
+        implementation("com.jeff-media:MorePersistentDataTypes:2.4.0")
 
         implementation("com.jeff-media:persistent-data-serializer:1.0")
         implementation("org.jetbrains:annotations:24.1.0") { isTransitive = false }
